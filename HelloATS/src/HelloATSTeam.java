eef059877f74d9f3dcb8a37193610985f2545400
E000542 <E000542@HW1579.cignitiglobal.com>
2017-07-25 14:48:14 +0530

    Commiting to Central Repository

---
+++ HelloATS/src/HelloATSTeam.java
@@ -0,0 +1,12 @@
+
+public class HelloATSTeam {
+	public static void main(String[] args) {
+	      // Prints "Hello World" in the terminal window.
+	      System.out.println("Hello, World");
+	      System.out.println("Hello, America");
+	      System.out.println("Hello, India");
+	      System.out.println("Hello, HYderabad");
+	      System.out.println("Hello, Bangalore");
+	      System.out.println("Hello, chennai");
+	   }
+}
